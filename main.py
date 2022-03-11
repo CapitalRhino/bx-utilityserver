4de93271e10af01ad0b76b3caf1d6ab2c685e0a9
lastvoidtemplar <60808936+lastvoidtemplar@users.noreply.github.com>
2022-03-11 21:41:10 +0200

    final

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import cv2
 import numpy as np
 from flask import Flask
 from flask_restful import Api,Resource
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 class BarcodeReader(Resource):
     def get(self,base64string):
