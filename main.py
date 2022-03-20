5f6a47bf847c20e086a638978114fd6f4c75e143
lastvoidtemplar <60808936+lastvoidtemplar@users.noreply.github.com>
2022-03-20 12:31:22 +0200

    func

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ import numpy as np
 from flask import Flask
 from flask_restful import Api,Resource
 from flask_cors import CORS
-
-
+from flask import request
+import ast
 app = Flask(__name__)
 CORS(app)
-api = Api(app)
-class BarcodeReader(Resource):
-    def get(self,base64string):
-        image = base64.urlsafe_b64decode(base64string)
-        decoded = cv2.imdecode(np.frombuffer(image, np.uint8), -1)
-        image_decode = pyzbar.decode(decoded)
-        if(len(image_decode)==0):
-            return {"status":404,"error":"barcode not found"}
-        data = image_decode[0].data
-        print(data)
-        return {"status":200,"barcodedata":data.decode('UTF-8')}
-
-api.add_resource(BarcodeReader,"/barcodereader/<string:base64string>")
+@app.route("/barcodereader", methods=["POST"])
+def barcodereader():
+    data = request.data.decode('utf-8')
+    data = ast.literal_eval(data)
+    base64string = data["image"]
+    image = base64.urlsafe_b64decode(base64string)
+    decoded = cv2.imdecode(np.frombuffer(image, np.uint8), -1)
+    image_decode = pyzbar.decode(decoded)
+    if(len(image_decode)==0):
+        return {"status":404,"error":"barcode not found"}
+    data = image_decode[0].data
+    print(data)
+    return {"status":200,"barcodedata":data.decode('UTF-8')}
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 
